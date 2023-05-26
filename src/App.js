a8e935ff5dce27fc356de73f165014239ff1028a
Mimi397 <izzena@hotmail.com>
2023-05-25 22:35:56 -0400

    May 25th

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 import React, { useState, useEffect } from "react";
 import CharacterSearch from "./component/CharacterSearch";
 import CharacterData from "./component/CharacterData";
@@ -11,9 +12,35 @@ import "./App.css";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+ 
 
   useEffect(() => {
-    axios.get("https://swapi.dev/api/people").then((response) => {
+    axios.get("https://swapi.dev/api/people")
+    .then((response) => {
+      // loop through each character
+      for (let index = 0; index < response.data.results.length; index++) {
+        const character = response.data.results[index];
+        // console.log(character)
+     
+
+        //get the homeworld name
+          const homeworldName = axios.get("https://swapi.dev/api/planets")
+          .then((response)=> {
+            setCharacters(response.data.result.name)
+          }, [])
+          console.log(homeworldName)
+
+           // assign the character's homeworldName property
+             character.homeworldName =  homeworldName.data.results;
+       
+     
+             // get the species name
+       const species = response.data.name
+       // assign the character's speciesName property
+       character.speciesName = "species"
+      //  "TBD2";
+       
+      }
       setCharacters(response.data.results);
     });
   }, []);
